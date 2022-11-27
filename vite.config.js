c2d463ffdaa60ed75df855b24a860c0d2aa52f15
eleftheros <eleftheros_all@protonmail.com>
2022-11-27 14:40:02 +0200

    add cloudflare pages package

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
-import cloudflare from "solid-start-cloudflare-workers";
+import cloudflare from "solid-start-cloudflare-pages";
 
 export default defineConfig({
   plugins: [solid({ adapter: cloudflare({}) })]
